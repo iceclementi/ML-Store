418f285832184545929e1f160cd7ec932b2dafd1
iceclementi <iceclementi@gmail.com>
2021-01-14 22:13:36 +0800

    Set up unity and add some hero images

---
+++ ML Store - Unity/Assets/Scripts/HeroData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class HeroData
+{
+    private string name;
+    private string description;
+    private int cost;
+    // private string[] roles;
+    // private string[] specialties;
+
+}
